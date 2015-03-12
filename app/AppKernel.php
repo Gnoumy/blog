34c295d3a9622736c8d6100d0fe306652ef191e7
Igmar <hugo.herblot@gmail.com>
2015-03-12 19:52:38 +0100

    install admin 1er part

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,12 @@ class AppKernel extends Kernel
             new blogBundle\blogBundle(),
 
             new FOS\UserBundle\FOSUserBundle(),
+
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
